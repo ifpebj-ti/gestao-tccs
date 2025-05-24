9f5bb10454db1a12a0aae48e3b08762ecfa73366
Gabriel <devzando8182@gmail.com>
2025-05-24 20:48:57 -0300

    adicionando configuraçãoes e migrations com ef

---
+++ backend/gestaotcc.Infra/Database/PostgresSql/Migrations/20250524233830_V2.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace gestaotcc.Infra.Database.PostgresSql.Migrations
+{
+    /// <inheritdoc />
+    public partial class V2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tccs",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Title = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: true),
+                    Summary = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: true),
+                    Status = table.Column<string>(type: "text", nullable: false),
+                    CreationDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tccs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TccInvites",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Email = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Code = table.Column<string>(type: "character varying(10)", maxLength: 10, nullable: false),
+                    IsValidCode = table.Column<bool>(type: "boolean", nullable: false),
+                    TccId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TccInvites", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TccInvites_Tccs_TccId",
+                        column: x => x.TccId,
+                        principalTable: "Tccs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserTccs",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<long>(type: "bigint", nullable: false),
+                    TccId = table.Column<long>(type: "bigint", nullable: false),
+                    BindingDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserTccs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserTccs_Tccs_TccId",
+                        column: x => x.TccId,
+                        principalTable: "Tccs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserTccs_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TccInvites_TccId",
+                table: "TccInvites",
+                column: "TccId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserTccs_TccId",
+                table: "UserTccs",
+                column: "TccId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserTccs_UserId",
+                table: "UserTccs",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TccInvites");
+
+            migrationBuilder.DropTable(
+                name: "UserTccs");
+
+            migrationBuilder.DropTable(
+                name: "Tccs");
+        }
+    }
+}
