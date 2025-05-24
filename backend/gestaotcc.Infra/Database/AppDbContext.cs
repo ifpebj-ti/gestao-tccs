8e498c4eb0088e61c5adb436befd8d8fe32f8813
Gabriel <devzando8182@gmail.com>
2025-05-24 20:48:57 -0300

    adicionando configuraçãoes e migrations com ef

--- backend/gestaotcc.Infra/Database/AppDbContext.cs
+++ backend/gestaotcc.Infra/Database/AppDbContext.cs
@@ -1,7 +1,10 @@
 using gestaotcc.Domain.Entities.AccessCode;
 using gestaotcc.Domain.Entities.Course;
 using gestaotcc.Domain.Entities.Profile;
+using gestaotcc.Domain.Entities.Tcc;
+using gestaotcc.Domain.Entities.TccInvite;
 using gestaotcc.Domain.Entities.User;
+using gestaotcc.Domain.Entities.UserTcc;
 using Microsoft.EntityFrameworkCore;
 
 namespace gestaotcc.Infra.Database;
@@ -12,6 +15,9 @@ public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(op
     public DbSet<ProfileEntity> Profiles { get; set; }
     public DbSet<AccessCodeEntity> AccessCodes { get; set; }
     public DbSet<CourseEntity> Courses { get; set; }
+    public DbSet<TccEntity> Tccs { get; set; }
+    public DbSet<UserTccEntity> UserTccs { get; set; }
+    public DbSet<TccInviteEntity> TccInvites { get; set; }
     
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
---
+++ backend/gestaotcc.Infra/Database/PostgresSql/Configuration/UserTccConfiguration.cs
@@ -0,0 +1,24 @@
+using gestaotcc.Domain.Entities.UserTcc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace gestaotcc.Infra.Database.PostgresSql.Configuration;
+
+public class UserTccConfiguration : IEntityTypeConfiguration<UserTccEntity>
+{
+    public void Configure(EntityTypeBuilder<UserTccEntity> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.BindingDate)
+            .IsRequired();
+
+        builder.HasOne(x => x.User)
+            .WithMany(x => x.UserTccs)
+            .HasForeignKey(x => x.UserId);
+        
+        builder.HasOne(x => x.Tcc)
+            .WithMany(x => x.UserTccs)
+            .HasForeignKey(x => x.TccId);
+    }
+}
