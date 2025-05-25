6e03b21f2297d630f068c4a29460cadb64f3a394
Mariana Cavalcanti Machado Anselmo <cmariana935@gmail.com>
2025-05-25 18:48:39 -0300

    feat: implementa formulário para nova proposta de TCC com validação de dados

---
+++ frontend/src/app/hooks/useNewTcc.ts
@@ -0,0 +1,66 @@
+'use client';
+
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { newTccSchema, NewTccSchemaType } from '@/app/schemas/newTccSchema';
+import { toast } from 'react-toastify';
+import { useEffect, useState } from 'react';
+
+export function useNewTccForm() {
+  const [advisors, setAdvisors] = useState<{ id: number; name: string }[]>([]);
+
+  const form = useForm<NewTccSchemaType>({
+    resolver: zodResolver(newTccSchema),
+    defaultValues: {
+      studentEmails: [''],
+      advisorId: 0,
+      title: '',
+      summary: '',
+    },
+  });
+
+  useEffect(() => {
+    const fetchAdvisors = async () => {
+      try {
+        const res = await fetch(process.env.NEXT_PUBLIC_API_URL + '/api/User/filter?Profile=ADVISOR');
+        if (!res.ok) throw new Error('Erro ao buscar orientadores');
+        const data = await res.json();
+        setAdvisors(data);
+      } catch (error) {
+        console.error(error);
+        toast.error('Erro ao carregar orientadores.');
+      }
+    };
+
+    fetchAdvisors();
+  }, []);
+
+  const submitForm: SubmitHandler<NewTccSchemaType> = async (data) => {
+    try {
+      const response = await fetch(process.env.NEXT_PUBLIC_API_URL + '/api/Tcc', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          studentEmails: data.studentEmails,
+          title: data.title,
+          summary: data.summary,
+          advisorId: data.advisorId,
+        }),
+      });
+      console.log('Resposta do servidor:', response);
+
+      if (response.ok) {
+        toast.success('Proposta de TCC enviada com sucesso!');
+      } else {
+        toast.error(`Erro na requisição: ${response.status} ${response.statusText}`);
+      }
+    } catch (error) {
+      toast.error('Erro ao enviar a proposta de TCC.');
+      console.error(error);
+    }
+  };
+
+  return { form, submitForm, advisors };
+}
