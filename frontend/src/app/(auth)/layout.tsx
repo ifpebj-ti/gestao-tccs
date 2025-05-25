19cca22cf7abe1ce52442126bf5b8cccfc0478d3
Mariana Cavalcanti Machado Anselmo <cmariana935@gmail.com>
2025-05-24 23:12:06 -0300

    feat: refactor HomePage component and add AuthRoutesLayout

---
+++ frontend/src/app/(auth)/layout.tsx
@@ -0,0 +1,14 @@
+import Header from '@/components/Header';
+
+export default function AuthRoutesLayout({
+  children
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <Header />
+      <main className="mt-30 md:mt-24 px-5">{children}</main>
+    </>
+  );
+}
