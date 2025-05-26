6d220bde0bd9c43c303e0364092ce8825cb4fadf
cLausx0 <david.almeida2707@gmail.com>
2025-05-25 21:18:43 -0300

    feat: Create students and teachers, associate TCCs and send their emails

---
+++ backend/gestaotcc.Application/Helpers/PasswordHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Cryptography;
+
+namespace gestaotcc.Application.Helpers;
+public static class PasswordHelper
+{
+    private static readonly char[] AvailableChars =
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+[]{}?".ToCharArray();
+
+    public static string GenerateRandomPassword(int length = 12)
+    {
+        using var rng = RandomNumberGenerator.Create();
+        var bytes = new byte[length];
+        rng.GetBytes(bytes);
+
+        var chars = new char[length];
+        for (int i = 0; i < length; i++)
+        {
+            chars[i] = AvailableChars[bytes[i] % AvailableChars.Length];
+        }
+
+        return new string(chars);
+    }
+}
