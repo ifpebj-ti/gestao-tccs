853b00bdccd0fe10ec0b0da215788b930917a9f6
Mariana Cavalcanti Machado Anselmo <cmariana935@gmail.com>
2025-06-01 17:11:08 -0300

    feat: add new user registration form with validation and update schema

---
+++ frontend/src/app/(auth)/newUser/page.tsx
@@ -0,0 +1,118 @@
+'use client';
+
+import { BreadcrumbAuto } from '@/components/ui/breadcrumb';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import {
+  faEnvelope,
+  faGraduationCap,
+  faIdCard,
+  faUser
+} from '@fortawesome/free-solid-svg-icons';
+import { useNewUserForm } from '@/app/hooks/useNewUser';
+
+export default function NewUser() {
+  const { errors, handleSubmit, register, submitForm, isSubmitting } =
+    useNewUserForm();
+
+  return (
+    <div className="flex flex-col">
+      <BreadcrumbAuto />
+      <h1 className="md:text-4xl text-3xl font-semibold md:font-normal mb-10">
+        Novo usuário
+      </h1>
+
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit(submitForm)}>
+        <h2 className="text-lg font-extrabold uppercase">
+          Informações do usuário
+        </h2>
+        <div className="grid md:grid-cols-2 gap-4">
+          <div className="grid items-center gap-1.5">
+            <Label className="font-semibold" htmlFor="name">
+              Nome
+            </Label>
+            <Input
+              id="name"
+              type="text"
+              placeholder="Digite o nome do usuário"
+              icon={faUser}
+              errorText={errors.name?.message?.toString()}
+              {...register('name')}
+            />
+          </div>
+          <div className="grid items-center gap-1.5">
+            <Label className="font-semibold" htmlFor="profile">
+              Perfil
+            </Label>
+            <select
+              id="profile"
+              required
+              {...register('profile')}
+              className="flex items-center border border-gray-400 bg-white rounded-xs px-3 py-2 focus-within:ring-2 focus-within:ring-blue-500 transition-all cursor-pointer"
+            >
+              <option value="">Selecione um perfil</option>
+              <option value="ADMIN">Administrador</option>
+              <option value="COORDINATOR">Coordenador</option>
+              <option value="SUPERVISOR">Supervisor</option>
+              <option value="ADVISOR">Orientador</option>
+              <option value="BANKING">Banca</option>
+              <option value="LIBRARY">Biblioteca</option>
+            </select>
+          </div>
+        </div>
+        <div className="grid md:grid-cols-3 gap-4">
+          <div className="grid items-center gap-1.5">
+            <Label className="font-semibold" htmlFor="email">
+              Email
+            </Label>
+            <Input
+              id="email"
+              type="email"
+              placeholder="Digite o email do usuário"
+              icon={faEnvelope}
+              errorText={errors.email?.message?.toString()}
+              {...register('email')}
+            />
+          </div>
+          {/* campo cpf */}
+          <div className="grid items-center gap-1.5">
+            <Label className="font-semibold" htmlFor="cpf">
+              CPF
+            </Label>
+            <Input
+              id="cpf"
+              type="text"
+              placeholder="Digite o CPF do usuário"
+              icon={faIdCard}
+              errorText={errors.cpf?.message?.toString()}
+              {...register('cpf')}
+            />
+          </div>
+          {/* campo siape */}
+          <div className="grid items-center gap-1.5">
+            <Label className="font-semibold" htmlFor="siape">
+              SIAPE
+            </Label>
+            <Input
+              id="siape"
+              type="text"
+              placeholder="Digite o SIAPE do usuário"
+              icon={faGraduationCap}
+              errorText={errors.siape?.message?.toString()}
+              {...register('siape')}
+            />
+          </div>
+        </div>
+        <div className="flex gap-2 md:self-end">
+          <Button variant="outline" className="w-full md:w-fit">
+            Cancelar
+          </Button>
+          <Button type="submit" className="w-full md:w-fit">
+            {isSubmitting ? 'Cadastrando...' : 'Cadastrar usuário'}
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+}
--- frontend/src/app/hooks/useNewUser.ts
+++ frontend/src/app/hooks/useNewUser.ts
@@ -2,41 +2,42 @@ import { SubmitHandler, useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { newUserSchema, NewUserSchemaSchemaType } from '@/app/schemas/newUserSchema';
 import { toast } from 'react-toastify';
+import { useRouter } from 'next/navigation';
 
 export function useNewUserForm() {
-  const {handleSubmit, register, formState: {errors}, reset} = useForm<NewUserSchemaSchemaType>({
+  const { push } = useRouter();
+  const {handleSubmit, register, formState: {errors, isSubmitting}, reset} = useForm<NewUserSchemaSchemaType>({
     resolver: zodResolver(newUserSchema),
     defaultValues: {
       name: '',
       email: '',
       registration: '',
       cpf: '',
-      profile: ['STUDENT'],
+      profile: undefined,
       siape: '',
       course: 'ENGENHARIA DE SOFTWARE'
     }
   });
 
   const submitForm: SubmitHandler<NewUserSchemaSchemaType> = async (data) => {
-    console.log('Dados do formulário:', data);
     try {
       const response = await fetch(process.env.NEXT_PUBLIC_API_URL + '/api/User', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify(data)
+        body: JSON.stringify({...data, profile: [data.profile]})
       });
 
-      console.log('Dados enviados:', data);
-      console.log('Resposta do servidor:', response);
       if (response.ok) {
         toast.success('Usuário registrado com sucesso!');
         reset();
         const result = await response.json();
         const expires = new Date(Date.now() + 5 * 60 * 1000).toUTCString();
         document.cookie = `access_token_temp=${result.token}; expires=${expires}; path=/; secure; samesite=Strict`;
-        window.location.href = '/newPassword';
+        if (data.profile === 'STUDENT') {
+          push('/newPassword');
+        }
       } else {
         const errorText = await response.text();
         toast.error(`Erro: ${response.status} - ${errorText}`);
@@ -47,5 +48,5 @@ export function useNewUserForm() {
     }
   };
 
-  return { register, submitForm, handleSubmit, errors};
+  return { register, submitForm, handleSubmit, errors, isSubmitting};
 }
