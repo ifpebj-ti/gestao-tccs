96c6749c9984368c25d7018f67fea7598d0401dd
Mariana Cavalcanti Machado Anselmo <cmariana935@gmail.com>
2025-06-08 14:49:14 -0300

    feat: add collapsible cards for mobile view in HomePage component

--- frontend/src/app/(auth)/homePage/page.tsx
+++ frontend/src/app/(auth)/homePage/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { CardHome } from '@/components/CardHome';
+import { CollapseCardMobile } from '@/components/CollapseCardMobile'; // novo componente
 import { BreadcrumbAuto } from '@/components/ui/breadcrumb';
 import {
   faFileCircleCheck,
@@ -13,6 +14,7 @@ import Link from 'next/link';
 import { jwtDecode } from 'jwt-decode';
 import Cookies from 'js-cookie';
 import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
 
 interface DecodedToken {
   unique_name: string;
@@ -21,6 +23,7 @@ interface DecodedToken {
 }
 
 export default function HomePage() {
+  const { push } = useRouter();
   const [profile, setProfile] = useState<string | null>(null);
 
   useEffect(() => {
@@ -40,107 +43,168 @@ export default function HomePage() {
         Página Inicial
       </h1>
 
+      {/* Mobile (Collapse) */}
+      <div className="md:hidden">
+        {canView([
+          'ADMIN',
+          'COORDINATOR',
+          'SUPERVISOR',
+          'ADVISOR',
+          'LIBRARY',
+          'BANKING',
+          'STUDENT'
+        ]) && (
+          <CollapseCardMobile
+            title="Assinaturas pendentes"
+            icon={faFileSignature}
+            indicatorNumber={3}
+            indicatorColor="bg-red-600"
+            onClick={() => push('/PendingSignatures')}
+          />
+        )}
+
+        {canView([
+          'ADMIN',
+          'COORDINATOR',
+          'SUPERVISOR',
+          'ADVISOR',
+          'LIBRARY',
+          'BANKING'
+        ]) && (
+          <CollapseCardMobile
+            title="TCCs em andamento"
+            icon={faGraduationCap}
+            indicatorNumber={7}
+            indicatorColor="bg-blue-600"
+            onClick={() => push('/OngoingTCCs')}
+          />
+        )}
+
+        {canView([
+          'ADMIN',
+          'COORDINATOR',
+          'SUPERVISOR',
+          'ADVISOR',
+          'LIBRARY'
+        ]) && (
+          <CollapseCardMobile
+            title="TCCs concluídos"
+            icon={faFileCircleCheck}
+            onClick={() => push('/CompletedTCCs')}
+          ></CollapseCardMobile>
+        )}
+
+        {canView(['STUDENT']) && (
+          <CollapseCardMobile
+            title="Meu TCC"
+            icon={faGraduationCap}
+            onClick={() => push('/TCC/{id}')}
+          ></CollapseCardMobile>
+        )}
+
+        {canView(['ADMIN', 'COORDINATOR', 'SUPERVISOR', 'ADVISOR']) && (
+          <CollapseCardMobile
+            title="Cadastrar nova proposta"
+            icon={faFileCirclePlus}
+            onClick={() => push('/newTCC')}
+          ></CollapseCardMobile>
+        )}
+
+        {canView(['ADMIN', 'COORDINATOR', 'SUPERVISOR']) && (
+          <CollapseCardMobile
+            title="Cadastrar novo usuário"
+            icon={faUserPlus}
+            onClick={() => push('/newUser')}
+          ></CollapseCardMobile>
+        )}
+      </div>
+
+      {/* Desktop (Grid Cards) */}
       <div
-        className={`grid gap-6 ${
+        className={`hidden md:grid gap-6 ${
           profile === 'STUDENT' || profile === 'BANKING'
             ? 'grid-cols-2'
-            : 'grid-cols-1 sm:grid-cols-2 md:grid-cols-3'
+            : 'md:grid-cols-2 lg:grid-cols-3'
         }`}
       >
-        {/* Assinaturas pendentes */}
-        <div
-          className={
-            canView([
-              'ADMIN',
-              'COORDINATOR',
-              'SUPERVISOR',
-              'ADVISOR',
-              'LIBRARY',
-              'BANKING',
-              'STUDENT'
-            ])
-              ? ''
-              : 'hidden'
-          }
-        >
+        {canView([
+          'ADMIN',
+          'COORDINATOR',
+          'SUPERVISOR',
+          'ADVISOR',
+          'LIBRARY',
+          'BANKING',
+          'STUDENT'
+        ]) && (
           <CardHome
             title="Assinaturas pendentes"
             icon={faFileSignature}
             indicatorNumber={3}
             indicatorColor="bg-red-600"
+            onClick={() => push('/PendingSignatures')}
           />
-        </div>
+        )}
 
-        {/* TCCs em andamento */}
-        <div
-          className={
-            canView([
-              'ADMIN',
-              'COORDINATOR',
-              'SUPERVISOR',
-              'ADVISOR',
-              'LIBRARY',
-              'BANKING'
-            ])
-              ? ''
-              : 'hidden'
-          }
-        >
+        {canView([
+          'ADMIN',
+          'COORDINATOR',
+          'SUPERVISOR',
+          'ADVISOR',
+          'LIBRARY',
+          'BANKING'
+        ]) && (
           <CardHome
             title="TCCs em andamento"
             icon={faGraduationCap}
             indicatorNumber={7}
             indicatorColor="bg-blue-600"
+            onClick={() => push('/OngoingTCCs')}
           />
-        </div>
+        )}
 
-        {/* TCCs concluídos */}
-        <div
-          className={
-            canView([
-              'ADMIN',
-              'COORDINATOR',
-              'SUPERVISOR',
-              'ADVISOR',
-              'LIBRARY'
-            ])
-              ? ''
-              : 'hidden'
-          }
-        >
-          <CardHome title="TCCs concluídos" icon={faFileCircleCheck} />
-        </div>
+        {canView([
+          'ADMIN',
+          'COORDINATOR',
+          'SUPERVISOR',
+          'ADVISOR',
+          'LIBRARY'
+        ]) && (
+          <CardHome
+            title="TCCs concluídos"
+            icon={faFileCircleCheck}
+            onClick={() => push('/CompletedTCCs')}
+          />
+        )}
 
-        {/* Meu TCC (somente estudante) */}
-        <div className={canView(['STUDENT']) ? '' : 'hidden'}>
+        {canView(['STUDENT']) && (
           <Link href="/meuTCC">
-            <CardHome title="Meu TCC" icon={faGraduationCap} />
+            <CardHome
+              title="Meu TCC"
+              icon={faGraduationCap}
+              onClick={() => push('/TCC/{id}')}
+            />
           </Link>
-        </div>
+        )}
 
-        {/* Cadastrar nova proposta */}
-        <div
-          className={
-            canView(['ADMIN', 'COORDINATOR', 'SUPERVISOR', 'ADVISOR'])
-              ? ''
-              : 'hidden'
-          }
-        >
+        {canView(['ADMIN', 'COORDINATOR', 'SUPERVISOR', 'ADVISOR']) && (
           <Link href="/newTCC">
-            <CardHome title="Cadastrar nova proposta" icon={faFileCirclePlus} />
+            <CardHome
+              title="Cadastrar nova proposta"
+              icon={faFileCirclePlus}
+              onClick={() => push('newTCC')}
+            />
           </Link>
-        </div>
+        )}
 
-        {/* Cadastrar novo usuário */}
-        <div
-          className={
-            canView(['ADMIN', 'COORDINATOR', 'SUPERVISOR']) ? '' : 'hidden'
-          }
-        >
+        {canView(['ADMIN', 'COORDINATOR', 'SUPERVISOR']) && (
           <Link href="/newUser">
-            <CardHome title="Cadastrar novo usuário" icon={faUserPlus} />
+            <CardHome
+              title="Cadastrar novo usuário"
+              icon={faUserPlus}
+              onClick={() => push('newUser')}
+            />
           </Link>
-        </div>
+        )}
       </div>
     </div>
   );
