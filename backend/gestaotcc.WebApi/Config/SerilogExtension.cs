0e21d72fc3234c9db60fc3399bfb4fdadf99c92c
Gabriel <devzando8182@gmail.com>
2025-06-13 09:04:39 -0300

    feat: adicionando grafana sink

--- backend/gestaotcc.WebApi/Config/SerilogExtension.cs
+++ backend/gestaotcc.WebApi/Config/SerilogExtension.cs
@@ -1,5 +1,6 @@
 using gestaotcc.WebApi.Logs;
 using Serilog;
+using Serilog.Sinks.Grafana.Loki;
 
 namespace gestaotcc.WebApi.Config;
 
@@ -7,12 +8,19 @@ public static class SerilogExtension
 {
     public static void AddSerilogExtension(this IHostBuilder hostBuilder)
     {
+        
         hostBuilder.UseSerilog((context, services, configuration) =>
         {
+            var environment = context.HostingEnvironment.EnvironmentName ?? "unknown";
+            
             configuration.ReadFrom.Configuration(context.Configuration)
                 .Enrich.FromLogContext()
                 .WriteTo.Console(new SimpleJsonLogFormatter())
-                .WriteTo.File(new SimpleJsonLogFormatter(), "Logs/log.txt", rollOnFileSizeLimit: true);
+                .WriteTo.GrafanaLoki("http://loki:3100", labels: new[]
+                {
+                    new LokiLabel { Key = "app", Value = "gestao-backend" },
+                    new LokiLabel { Key = "env", Value = environment }
+                });
         });
     }
 }
