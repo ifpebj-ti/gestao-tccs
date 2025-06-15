59496e2dda4d4d892e36821818f526cf1d2c440e
Gabriel <devzando8182@gmail.com>
2025-06-14 21:59:03 -0300

    feat: adicionando null para profileId

---
+++ backend/gestaotcc.Infra/Database/PostgresSql/Migrations/20250615005222_AddNullFieldProfileId.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace gestaotcc.Infra.Database.PostgresSql.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNullFieldProfileId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserTccs_Profiles_ProfileId",
+                table: "UserTccs");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "ProfileId",
+                table: "UserTccs",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserTccs_Profiles_ProfileId",
+                table: "UserTccs",
+                column: "ProfileId",
+                principalTable: "Profiles",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserTccs_Profiles_ProfileId",
+                table: "UserTccs");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "ProfileId",
+                table: "UserTccs",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserTccs_Profiles_ProfileId",
+                table: "UserTccs",
+                column: "ProfileId",
+                principalTable: "Profiles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
