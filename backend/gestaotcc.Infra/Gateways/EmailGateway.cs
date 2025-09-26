8bb089ce099ca874669c5abe0850a4cb0fc5d03c
Gabriel <devzando8182@gmail.com>
2025-09-26 01:32:42 -0300

    feat: adicionando correção url front

--- backend/gestaotcc.Infra/Gateways/EmailGateway.cs
+++ backend/gestaotcc.Infra/Gateways/EmailGateway.cs
@@ -63,11 +63,6 @@ public class EmailGateway(IConfiguration configuration) : IEmailGateway
     
     private MailMessage CreateMessage(SendEmailDTO data)
     {
-        var corsSettings = configuration.GetSection("CORS_SETTINGS");
-        var urlfront = corsSettings.GetValue<string>("URL_FRONT");
-        
-        data.Variables.Add("urlfront", urlfront);
-        
         var mailSettings = configuration.GetSection("MailSettings");
         var emailId = mailSettings.GetValue<string>("EmailId");
         
@@ -83,6 +78,11 @@ public class EmailGateway(IConfiguration configuration) : IEmailGateway
 
     private string CreateTemplate(SendEmailDTO data)
     {
+        var corsSettings = configuration.GetSection("CORS_SETTINGS");
+        var urlfront = corsSettings.GetValue<string>("URL_FRONT");
+        
+        data.Variables.Add("urlfront", urlfront);
+        
         var emailBody = GetFileTemplate(data.TypeTemplate);
         var template = Template.Parse(emailBody);
 
