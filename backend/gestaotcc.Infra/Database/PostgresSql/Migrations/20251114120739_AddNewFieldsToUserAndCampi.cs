6e49e689392e96d66290779f988a563b65419628
Gabriel <devzando8182@gmail.com>
2025-11-14 09:37:42 -0300

    feat: adicionando migrations de novos campos para usuário e campi

---
+++ backend/gestaotcc.Infra/Database/PostgresSql/Migrations/20251114120739_AddNewFieldsToUserAndCampi.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace gestaotcc.Infra.Database.PostgresSql.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNewFieldsToUserAndCampi : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Phone",
+                table: "Users",
+                type: "character varying(15)",
+                maxLength: 15,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Shift",
+                table: "Users",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Titration",
+                table: "Users",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserClass",
+                table: "Users",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "Campi",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Phone",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Shift",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Titration",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "UserClass",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "Campi");
+        }
+    }
+}
