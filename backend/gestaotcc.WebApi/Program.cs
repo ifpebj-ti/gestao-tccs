cf0390fd2be7078459c2343e0cec0c81d56e49ff
Gabriel <devzando8182@gmail.com>
2025-11-20 11:49:32 -0300

    fix: corrigindo problema da busca de documento

--- backend/gestaotcc.WebApi/Program.cs
+++ backend/gestaotcc.WebApi/Program.cs
@@ -38,7 +38,7 @@ builder.Services.AddMinioExtension(builder.Configuration, builder.Environment);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
